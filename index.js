5a6bc5f0f362d77be874f7285e6bf7d0e58b0bd5
aarongill1 <aaron.gill@digital.homeoffice.gov.uk>
2023-05-30 14:56:56 +0100

    Renamed shift functions to more accurately describe function

--- index.js
+++ index.js
@@ -4,21 +4,21 @@ const { charToInt, intToChar } = require('./helpers');
 
 const N = 26;
 
-const shiftChar = (char, shift) => char === ' ' ? ' ' : intToChar((charToInt(char) + shift) % N);
+const shiftCharUp = (char, shift) => char === ' ' ? ' ' : intToChar((charToInt(char) + shift) % N);
 
-const shiftCharBack = (char, shift) => char === ' ' ? ' ' : intToChar((charToInt(char) - shift + N) % N);
+const shiftCharDown = (char, shift) => char === ' ' ? ' ' : intToChar((charToInt(char) - shift + N) % N);
 
 const encrypt = (message, shift) => (
   message
     .split('')
-    .map(char => shiftChar(char, shift) )
+    .map(char => shiftCharUp(char, shift) )
     .join('')
 );
 
 const decrypt = (message, shift) => (
   message
     .split('')
-    .map(char => shiftCharBack(char, shift))
+    .map(char => shiftCharDown(char, shift))
     .join('')
 );
 
