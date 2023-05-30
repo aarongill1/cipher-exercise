cee30e5856e6e39943b7e21df1ec1c8d99a5945e
aarongill1 <aaron.gill@digital.homeoffice.gov.uk>
2023-05-30 14:38:02 +0100

    Updated encrypt/decrypt interface to accept a user defined shift

--- index.spec.js
+++ index.spec.js
@@ -31,61 +31,61 @@ describe('cipher-exercise', () => {
 
   describe('encrypt', () => {
     it('is a function', () => expect(typeof encrypt).toBe('function'));
-    it('that takes 1 parameter', () => expect(encrypt).toHaveLength(1));
+    it('that takes 2 parameters', () => expect(encrypt).toHaveLength(2));
 
-    describe('when called', () => {
+    describe('when called with a shift of 2 characters', () => {
       let result;
+      let shift = 2;
+      
+        describe('with a simple message', () => {
+          beforeEach(() => {
+            result = encrypt(simple.message, shift);
+          });
 
-      describe('with a simple message', () => {
-        beforeEach(() => {
-          result = encrypt(simple.message);
+          it('returns a string', () => expect(typeof result).toBe('string'));
+          it('in which the letters are shifted by 2', () => expect(result).toBe(simple.shifted));
         });
 
-        it('returns a string', () => expect(typeof result).toBe('string'));
-        it('in which the letters are shifted by 2', () => expect(result).toBe(simple.shifted));
-      });
+        describe('with a tricky message', () => {
+          beforeEach(() => {
+            result = encrypt(tricky.message, shift);
+          });
 
-      describe('with a tricky message', () => {
-        beforeEach(() => {
-          result = encrypt(tricky.message);
+          it('returns a string', () => expect(typeof result).toBe('string'));
+          it('in which the letters are shifted by 2 and wrap', () => expect(result).toBe(tricky.shifted));
         });
 
-        it('returns a string', () => expect(typeof result).toBe('string'));
-        it('in which the letters are shifted by 2 and wrap', () => expect(result).toBe(tricky.shifted));
-      });
+        describe('with a multi-word message', () => {
+          beforeEach(() => {
+            result = encrypt(words.message, shift);
+          });
 
-      describe('with a multi-word message', () => {
-        beforeEach(() => {
-          result = encrypt(words.message);
+          it('returns a string', () => expect(typeof result).toBe('string'));
+          it('in which the letters (but NOT spaces) are shifted by 2', () => expect(result).toBe(words.shifted));
         });
 
-        it('returns a string', () => expect(typeof result).toBe('string'));
-        it('in which the letters (but NOT spaces) are shifted by 2', () => expect(result).toBe(words.shifted));
-      });
+        describe('with a complex message', () => {
+          beforeEach(() => {
+            result = encrypt(complex.message, 2);
+          });
 
-      describe('with a complex message', () => {
-        beforeEach(() => {
-          result = encrypt(complex.message);
+          it('returns a string', () => expect(typeof result).toBe('string'));
+          it('in which the letters are shifted by 2', () => expect(result).toBe(complex.shifted));
         });
-
-        it('returns a string', () => expect(typeof result).toBe('string'));
-        it('in which the letters are shifted by 2', () => expect(result).toBe(complex.shifted));
       });
-    });
   });
 
   describe('decrypt', () => {
     it('is a function', () => expect(typeof decrypt).toBe('function'));
-    it('that takes 1 parameter', () => expect(decrypt).toHaveLength(1));
+    it('that takes 2 parameters', () => expect(decrypt).toHaveLength(2));
 
-    describe('when called, reverses the effects of encrypt', () => {
+    describe('when called, reverses the effects of encrypt if the same character shift is applied', () => {
       let result;
+      let shift = 2
 
       describe('with a simple message', () => {
         beforeEach(() => {
-          result = decrypt(
-            encrypt(simple.message)
-          );
+          result = decrypt(encrypt(simple.message, 2), 2);
         });
 
         it('restores the message', () => expect(result).toBe(simple.message));
@@ -93,9 +93,7 @@ describe('cipher-exercise', () => {
 
       describe('with a tricky message', () => {
         beforeEach(() => {
-          result = decrypt(
-            encrypt(tricky.message)
-          );
+          result = decrypt(encrypt(tricky.message, 2), 2);
         });
 
         it('restores the message', () => expect(result).toBe(tricky.message));
@@ -103,9 +101,7 @@ describe('cipher-exercise', () => {
 
       describe('with a multi-word message', () => {
         beforeEach(() => {
-          result = decrypt(
-            encrypt(words.message)
-          );
+          result = decrypt(encrypt(words.message, 2), 2);
         });
 
         it('restores the message', () => expect(result).toBe(words.message));
@@ -113,9 +109,7 @@ describe('cipher-exercise', () => {
 
       describe('with a complex message', () => {
         beforeEach(() => {
-          result = decrypt(
-            encrypt(complex.message)
-          );
+          result = decrypt(encrypt(complex.message, 2), 2);
         });
 
         it('restores the message', () => expect(result).toBe(complex.message));
